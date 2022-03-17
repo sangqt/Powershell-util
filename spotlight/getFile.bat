01fd4a15e4ec036cd61e96030854c153c2f545e4
Sang Tran <sangqt.inb@gmail.com>
2022-03-17 12:54:21 +0900

    Create getFile.bat

---
+++ spotlight/getFile.bat
@@ -0,0 +1,3 @@
+set targetPath=%userprofile%\Pictures\Spotlight
+set assetPath=%userprofile%\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets
+copy %assetPath%\*.* %targetPath%\*.jpg
