55848f7a52f4e17e10c188b4d7c9d62b9938ea37
woraln <sangqt.inb@gmail.com>
2022-09-27 16:21:03 +0900

    Add hello worl and getting start

---
+++ learn/GettingStarted.ps1
@@ -0,0 +1,4 @@
+$date = Read-Host -Prompt "Please enter today date"
+$name = Read-Host -Prompt "Please enter your name"
+Write-Output "Today's date is $date."
+Write-Output "Today is the day $name began their PowerShell programming journey."
---
+++ learn/HelloWorld.ps1
@@ -0,0 +1,3 @@
+# Write-Output 'Hello World!'
+$name = Read-Host -Prompt "Please enter your name"
+Write-Output "Congratulations $name! You have written your first code with PowerShell!"
